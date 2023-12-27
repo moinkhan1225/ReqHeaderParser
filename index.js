373ba4537d8911e76f449a20625ffc6a63920f15
Hakai <moinkhanrobin1@gmail.com>
2023-12-28 01:44:07 +0530

    Request Header Parser Microservice

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  ipAdd=`192.168.137.1`;
+  res.json({ipaddress:ipAdd});
 });
 
 // listen for requests :)
